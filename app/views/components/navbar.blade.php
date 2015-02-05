f838a082c4c5699a1f555fa13841cf2b448b1736
Guy Cook <guy@ivorydungeon.net>
2015-02-05 17:13:11 +1300

    Finalise mobile styles

--- app/views/components/navbar.blade.php
+++ app/views/components/navbar.blade.php
@@ -1,11 +1,17 @@
 <nav class="navbar">
 	<section class="container">
 		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-links">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
 			<a class="navbar-brand" href="/">
-        {{ HTML::image('images/logo-colourx2.png', 'Calatex')}}
-      </a>
+				{{ HTML::image('images/logo-colourx2.png', 'Calatex')}}
+			</a>
 		</div>
-		<div class="collapse navbar-collapse" id="navbar-links">
+		<div class="navbar-collapse collapse" id="navbar-links">
 			<ul class="nav navbar-nav navbar-right">
 				@foreach([
 					'/' => 'Home',
