2b1d0842fd3ac4f7c667cbf2cdfa072500fc04d5
Joshua Gagnon <josh.n.gagnon@gmail.com>
2015-06-08 10:35:23 +1200

    Bios now justified

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -41,7 +41,7 @@
 				<img src="/images/tamina.jpg" alt="Tamina Cunningham-Adams">
 			</div>
 			<h3><span class="highlight">Tamina Cunningham-Adams</span><br>LLB (Hons), BA, B Ed (Tchg)</h3>
-			<p>
+			<p class="blurb">
 				Tamina is a banking and finance lawyer and practising liquidator.&nbsp; She specialises in corporate and personal insolvency law, securities enforcement and litigation.&nbsp; Before commencing practice as a liquidator, Tamina was a specialist litigator for top tier law firm, Simpson Grierson, for several years.
 			</p>
 			<p class="blurb">
@@ -56,7 +56,7 @@
 				<img src="/images/thomas.jpg" alt="Thomas Bloy">
 			</div>
 			<h3><span class="highlight">Thomas Bloy</span><br>LLB</h3>
-			<p>
+			<p class="blurb">
 				Thomas specialises in company, commercial, and contract law, but also has expertise in mergers and acquisitions, capital raising, and insolvency.&nbsp;Before going in-house at CataLex, he worked for Simpson Grierson’s corporate advisory and banking litigation teams. He has advised several NZX50 companies and has Court of Appeal litigation experience. Before becoming a lawyer, Thomas was an operations analyst for TOWER Insurance, specialising in system administration, process improvement, operational efficiency, and quality assurance.
 			</p>
 		</div>
@@ -65,10 +65,10 @@
 				<img src="/images/josh.jpg" alt="Josh Gagnon">
 			</div>
 			<h3><span class="highlight">Josh Gagnon</span><br>BSc (Hons), BA</h3>
-			<p>
+			<p class="blurb">
 				Josh is CataLex’s Chief Technology Officer. He is a software engineer that has worked in bioinformatics for the University of Otago.&nbsp; Prior to joining CataLex, Josh developed GIS (Geographic Information System) software for an Otago-based company.
 			</p>
-			<p>
+			<p class="blurb">
 				Josh's passion is developing web-based applications, and he loves the opportunity to contribute to others' projects as well as work on his own.
 			</p>
 		</div>
